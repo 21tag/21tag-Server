5604707a47d5189f82724930905babf3e00f046f
David Brodsky <davidpbrodsky@gmail.com>
2012-02-07 11:01:48 -0800

    /getteam GET and POST

--- totag/tag/views.py
+++ totag/tag/views.py
@@ -3,27 +3,33 @@ from django.http import HttpResponse
 import json
 from django.contrib.auth.models import User
 from models import Team
-import string
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def getTeam(request):
-	team = Team.objects.all()
-	#jdilly = jsonpickle.encode(team)
-	#response -> {"teams":[...]}
-	response = {}
-	teams = []
-	for t in team:
-		tdic = {}
-		tdic["id"] = t.name
-		users = []
-		#Get all users on team
-		for user in User.objects.filter(team__name__exact=t.name):
-			users.append(user.pk)
-		tdic["users"] = users
-		venues = []
-		for venue in t.venues.all():
-			venues.append(venue.pk)
-		tdic["venues"] = venues
-		teams.append(tdic)
-	response["teams"] = teams
-	json_response = json.dumps(response)
-	return HttpResponse(json_response, content_type="text/plain")
+    if request.method == "POST":
+        print request.POST["team"]
+        try:
+            team = [Team.objects.get(name=str(request.POST["team"]))]
+        except Exception, e:
+            team = None
+            print "error " + str(e)
+    else:
+        team = Team.objects.all()
+    response = {}
+    teams = []
+    for t in team:
+        tdic = {}
+        tdic["id"] = t.name
+        users = []
+        #Get all users on team
+        for user in User.objects.filter(team__name__exact=t.name):
+            users.append(user.pk)
+        tdic["users"] = users
+        venues = []
+        for venue in t.venues.all():
+            venues.append(venue.pk)
+        tdic["venues"] = venues
+        teams.append(tdic)
+    response["teams"] = teams
+    return HttpResponse(json.dumps(response), mimetype="application/json")
