ad0cd6964a08b636fb9b5be55de92d4ce491c632
David Brodsky <davidpbrodsky@gmail.com>
2012-02-28 12:01:42 -0800

    user posting

--- totag/tag/api.py
+++ totag/tag/api.py
@@ -7,6 +7,8 @@ from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
 from tastypie.constants import ALL
 from tastypie.serializers import Serializer
+from tastypie.exceptions import BadRequest
+from django.db import IntegrityError
 
 
 class TeamResource(ModelResource):
@@ -32,7 +34,16 @@ class TeamResource(ModelResource):
         bundle.data['members'] = plist
         return bundle
 
+# Not needed
+#class UserProfileResource(ModelResource):
+#    class Meta:
+#        queryset = UserProfile.objects.all()
+#        resource_name = 'userprofile'
+#        include_resource_uri = False
+#        include_absolute_url = False
+
 class UserResource(ModelResource):
+    #profile = fields.OneToOneField(UserProfileResource, 'get_profile', full=True)
     class Meta:
         queryset = User.objects.all()
         resource_name = 'user'
@@ -44,12 +55,34 @@ class UserResource(ModelResource):
         serializer = Serializer()
 
     def dehydrate(self, bundle):
-        profile = UserProfile.objects.get(pk=bundle.obj.pk)
+        profile = UserProfile.objects.get(pk=bundle.obj.pk)  # check that user and profile pks are safe to assume equal
         bundle.data['team'] = profile.team.pk
         bundle.data['teamname'] = profile.team.name
         bundle.data['currentVenueTime'] = profile.currentVenueTime
         bundle.data['currentVenueLastTime'] = profile.currentVenueLastTime
         bundle.data['currentVenueName'] = profile.currentVenueName
+        return bundle
+
+
+    def obj_create(self, bundle, request=None, **kwargs):
+        # set facebook id as username, fbauthcode as pw
+        # improve this security later
+        try:
+            fname, lname, username, password, email = bundle.data['firstname'], bundle.data['lastname'], bundle.data['fid'], bundle.data['fbauthcode'], bundle.data['email']
+        except:
+            raise BadRequest('Incomplete user data')
+        try:
+            bundle.obj = User.objects.create_user(username, email, password)
+            bundle.obj.first_name = fname
+            bundle.obj.last_name = lname
+            bundle.obj.save()
+            profile = UserProfile.objects.get(pk=bundle.obj.pk)  # check that user and profile pks are safe to assume equal
+            profile.fb_authcode = password
+            profile.fid = username
+            profile.save()
+        except IntegrityError, e:
+            print e
+            raise BadRequest('That username already exists')
         return bundle
 
 
